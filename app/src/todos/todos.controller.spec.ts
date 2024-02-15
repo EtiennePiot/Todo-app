aa156144cc772e94070d82eef14bba42569df2fa
etienne <etienne.piotrowksi@epitech.eu>
2024-02-15 11:40:26 +0100

    [ADD] Add some tests

--- app/src/todos/todos.controller.spec.ts
+++ app/src/todos/todos.controller.spec.ts
@@ -1,18 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodosController } from './todos.controller';
+import { TodosService } from './todos.service';
+import { Todo } from './todo.model';
 
 describe('TodosController', () => {
   let controller: TodosController;
+  let service: TodosService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodosController],
+      providers: [TodosService],
     }).compile();
 
     controller = module.get<TodosController>(TodosController);
+    service = module.get<TodosService>(TodosService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAllTodos', () => {
+    it('should return an array of todos', async () => {
+      const result: Todo[] = [{ id: 'a', title: 'Test Todo', done: false }];
+      jest.spyOn(service, 'findAllTodos').mockImplementation(() => result);
+  
+      expect(await controller.findAllTodos()).toBe(result);
+    });
+  });
+  
+  describe('findOneTodo', () => {
+    it('should return a single todo item', async () => {
+      const result: Todo = { id: 'a', title: 'Test Todo', done: false };
+      jest.spyOn(service, 'findOneTodo').mockImplementation(() => result);
+  
+      expect(await controller.findOneTodo('a')).toBe(result);
+    });
+  });
+  
+  describe('createTodo', () => {
+    it('should create a new todo item', async () => {
+      const result: Todo = { id: 'a', title: 'Test Todo', done: false };
+      jest.spyOn(service, 'createTodo').mockImplementation(() => result);
+  
+      expect(await controller.createTodo('Test Todo')).toBe(result);
+    });
+  });
+  
+  describe('updateTodo', () => {
+    it('should update a todo item', async () => {
+      const result: Todo = { id: 'a', title: 'Updated Test Todo', done: true };
+      jest.spyOn(service, 'updateTodo').mockImplementation(() => result);
+  
+      expect(await controller.updateTodo('a', 'Updated Test Todo', true)).toBe(result);
+    });
+  });
+  
+  describe('deleteTodo', () => {
+    it('should delete a todo item', async () => {
+      const spy = jest.spyOn(service, 'deleteTodo').mockImplementation(() => {});
+      await controller.deleteTodo('a');
+      expect(spy).toHaveBeenCalledWith('a');
+    });
   });
 });
--- app/src/todos/todos.service.spec.ts
+++ app/src/todos/todos.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodosService } from './todos.service';
+import { Todo } from './todo.model';
 
 describe('TodosService', () => {
   let service: TodosService;
@@ -12,7 +13,52 @@ describe('TodosService', () => {
     service = module.get<TodosService>(TodosService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createTodo', () => {
+    it('should create a new todo item', () => {
+      const title = 'Test Todo';
+      const todo = service.createTodo(title);
+      expect(todo.title).toEqual(title);
+      expect(todo.done).toEqual(false);
+      expect(typeof todo.id).toBe('string');
+    });
   });
+  
+  describe('findOneTodo', () => {
+    it('should return a single todo item', () => {
+      const todo = service.createTodo('Test Todo');
+      expect(service.findOneTodo(todo.id)).toEqual(todo);
+    });
+  
+    it('should throw an error if todo is not found', () => {
+      expect(() => service.findOneTodo('non-existing-id')).toThrow(Error);
+    });
+  });
+  
+  describe('updateTodo', () => {
+    it('should update a todo item', () => {
+      const todo = service.createTodo('Test Todo');
+      const updatedTodo = service.updateTodo(todo.id, 'Updated Test Todo', true);
+      expect(updatedTodo.title).toEqual('Updated Test Todo');
+      expect(updatedTodo.done).toBe(true);
+    });
+  
+    it('should throw an error if todo to update is not found', () => {
+      expect(() => service.updateTodo('non-existing-id', 'Test', true)).toThrow(Error);
+    });
+  });
+  
+  describe('deleteTodo', () => {
+    it('should delete a todo item', () => {
+      const todo = service.createTodo('Test Todo');
+      expect(service.findAllTodos()).toContain(todo);
+      service.deleteTodo(todo.id);
+      expect(service.findAllTodos()).not.toContain(todo);
+    });
+  
+    it('should throw an error if todo to delete is not found', () => {
+      expect(() => service.deleteTodo('non-existing-id')).toThrow(Error);
+    });
+  });
+  
+
 });
